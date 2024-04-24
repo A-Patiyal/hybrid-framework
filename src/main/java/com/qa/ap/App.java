b40469cd7edd97998851f02811e5ca6d62f22586
Anuj Patiyal <Anujpatiyal@gmail.com>
2024-04-24 13:48:15 +0530

    Created maven project via cmd-line

---
+++ src/main/java/com/qa/ap/App.java
@@ -0,0 +1,13 @@
+package com.qa.ap;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
